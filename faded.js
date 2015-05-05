6d0b3e82a669fb41b6b9158c2ebe844c34b2f3d7
Alex Vlasov <indifferentalex@outlook.com>
2015-05-06 01:15:43 +0200

    ready

---
+++ faded.js
@@ -0,0 +1,19 @@
+jQuery(function() {
+	if ($(".photo-desktop").is(":visible")) {
+		$(".brief").width($(".photo-desktop").width());
+		$(".words-and-letters").width($(".photo-desktop").width());
+		$(".posts").css({ "margin-left": $(".photo-desktop").width() });
+	}
+
+	$(".words-and-letters").transition({ opacity: 1, duration: 1500, delay: 500 });
+
+	var correctPostLeftMargin = $(".post").css("margin-left");
+
+	$(".post").css({ marginLeft: "20%" });
+
+  var delay = 500;
+  $(".post").each(function() {
+  	$(this).transition({ opacity: 1, marginLeft: correctPostLeftMargin, duration: 1000, delay: delay });
+    delay += 1000;
+  });	
+});
